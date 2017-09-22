abd636c89c91f7c569e84f26e8cd74309c164888
Jake Varness <jake.varness@gmail.com>
2017-09-21 20:41:55 -0500

    Monitor all *32*.* files

---
+++ VB.NET/Impact.vb
@@ -0,0 +1,27 @@
+﻿' cerner_2^5_2017
+' Ever wanted to monitor all your files that contain 32? Well look no further!
+Imports System.IO
+Namespace Engineering
+    Public Class Impact
+        Public Shared Sub Main()
+            Dim fsWatcher As New FileSystemWatcher
+            fsWatcher.Filter = "*32*.*"
+            fsWatcher.Path = "C:\"
+            fsWatcher.NotifyFilter = (NotifyFilters.Attributes Or NotifyFilters.CreationTime Or NotifyFilters.DirectoryName Or NotifyFilters.FileName Or NotifyFilters.LastAccess Or NotifyFilters.LastWrite Or NotifyFilters.Security Or NotifyFilters.Size)
+            AddHandler fsWatcher.Changed, AddressOf OnChanged
+            AddHandler fsWatcher.Created, AddressOf OnChanged
+            AddHandler fsWatcher.Deleted, AddressOf OnChanged
+            AddHandler fsWatcher.Renamed, AddressOf OnRenamed
+            fsWatcher.EnableRaisingEvents = True
+            fsWatcher.IncludeSubdirectories = True
+            Console.WriteLine("Press any key to quit . . .")
+            Console.ReadKey()
+        End Sub
+        Private Shared Sub OnChanged(sender As Object, eventArgs As FileSystemEventArgs)
+            Console.WriteLine(eventArgs.Name & " was " & [Enum].GetName(GetType(WatcherChangeTypes), eventArgs.ChangeType))
+        End Sub
+        Private Shared Sub OnRenamed(sender As Object, eventArgs As RenamedEventArgs)
+            Console.WriteLine(eventArgs.OldName & " was Renamed to " & eventArgs.Name)
+        End Sub
+    End Class
+End Namespace
---
+++ VB.NET/README.md
@@ -0,0 +1,10 @@
+# Monitor all of your directories and files with 32 in the name.
+
+To test, open a Visual Studio Developer Prompt and execute:
+
+```
+vbc Impact.vb
+Impact.exe
+```
+
+Then watch all of your \*32\*.\* files flood your command prompt!
