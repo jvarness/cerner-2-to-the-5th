2a77023238ff5f9d9ecdf11e2fb9ae7f46f74b6b
Jake Varness <jake.varness@gmail.com>
2017-09-21 22:48:59 -0500

    Elm SPA that will tell you how many github followers a user has

---
+++ Elm/Main.elm
@@ -0,0 +1,32 @@
+-- cerner_2^5_2017
+-- Ever wonder how many repos you own on 
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http exposing (..)
+import Json.Decode as Decode exposing (..)
+main = Html.program { init = (model, Cmd.none), view = view, update = update, subscriptions = (\_ -> Sub.none) }
+type Msg = UserId String | Submit | GithubInfoRetrieved (Result Http.Error Int)
+model = { user = "", followers = 0, err = "" }
+view model =
+    div [] [
+        input [ type_ "text", placeholder "Github Username", onInput UserId ] []
+        , button [ onClick Submit ] [ text "Submit" ]
+        , div [ ] [ text ("Number of Github followers: " ++ toString model.followers) ]
+        , div [ ] [ text model.err ] 
+    ]
+getGithubInfo userId =
+    Http.send GithubInfoRetrieved <| githubRequest ("http://api.github.com/users/" ++ model.user)
+githubRequest url =
+    Http.request 
+        { method = "GET", url = url, body = emptyBody, expect = expectJson (Decode.field "followers" <| Decode.int), timeout = Nothing, withCredentials = False, headers = [ Http.header "Accept" "application/json"] }
+update msg model =
+    case msg of
+        UserId id ->
+            ({ model | user = id }, Cmd.none)
+        Submit ->
+            ({model | err = "" }, getGithubInfo model.user)
+        GithubInfoRetrieved (Ok followers) ->
+            ({ model | followers = followers }, Cmd.none)
+        GithubInfoRetrieved (Err err) ->
+            ({ model | err = toString err }, Cmd.none)
