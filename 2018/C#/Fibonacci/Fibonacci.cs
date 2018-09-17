a109c5930cad64527bba3396c4e94e5fc52c60f7
Jake Varness <jake.varness@gmail.com>
2018-09-16 20:11:46 -0500

    Day four submission: fibonacci.NET

---
+++ 2018/C#/Fibonacci/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// cerner_2^5_2018
+namespace Fibonacci
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(args[0]);
+            if (number < 0)
+                throw new ArgumentException("The first argument in this program needs to be a positive number.");
+            Console.WriteLine("Fibonacci number " + number + " is: " + CalculateFibonacci(0, 1, number));
+        }
+
+        private static int CalculateFibonacci(int term1, int term2, int iterations)
+        {
+            if (iterations == 0) 
+            {
+                return 1;
+            }
+            else
+            {
+                return CalculateFibonacci(term2, term1 + term2, iterations-1) + term1;
+            }
+        }
+    }
+}
---
+++ 2018/C#/Fibonacci/README.md
@@ -0,0 +1,13 @@
+# Fibonacci
+
+That famous old sequence of numbers, available to you at the speed of `dotnet`!
+
+# How to run
+
+First, install [`dotnet`](https://www.microsoft.com/net), then on a command prompt run the following command:
+
+```
+dotnet run -- X
+```
+
+Where `X` is a positive integer value.
