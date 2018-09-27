85a92d85b5bd748e1dab59e8536237f46a42ca55
Jake Varness <jake.varness@gmail.com>
2018-09-26 21:32:52 -0500

    Day thirteen: elm 0.19

---
+++ 2018/Elm/dot-nineteen/README.md
@@ -0,0 +1,13 @@
+# dot-nineteen
+
+Elm recently released a new version! [0.19!!](https://elm-lang.org/blog/small-assets-without-the-headache)
+
+This is a very small program that demonstrates how to make an Elm project.
+
+# How to run
+
+Install [Elm](https://guide.elm-lang.org/install.html), then run:
+
+```
+elm reactor
+```
---
+++ 2018/Elm/dot-nineteen/src/Main.elm
@@ -0,0 +1,30 @@
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+
+main = Browser.sandbox { init = init, update = update, view = view }
+
+type alias Model = 
+  { count: Int,
+    text: String
+  } 
+
+type Msg = Increment | Decrement
+
+init = { count = 0, text = "Hello World!"}
+
+view model =
+    div [] [
+      text model.text,
+      button [ onClick Increment ] [ text "+" ],
+      text <| String.fromInt model.count,
+      button [ onClick Decrement ] [ text "-" ]
+    ]
+
+update msg model =
+  case msg of
+      Increment ->
+          { model | count = model.count + 1 }
+      Decrement ->
+          { model | count = model.count - 1 }
